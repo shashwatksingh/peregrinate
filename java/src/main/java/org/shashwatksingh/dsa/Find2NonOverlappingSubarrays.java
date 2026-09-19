b4ddc85f4f87eaa3213ff660b9aca401d7db9faa
shashwatksingh <shashwatksingh.27@gmail.com>
2026-09-19 10:02:19 +0530

    leetcode - find overlapping subarray greater than or equal to target

---
+++ java/src/main/java/org/shashwatksingh/dsa/Find2NonOverlappingSubarrays.java
@@ -0,0 +1,56 @@
+package org.shashwatksingh.dsa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*Find Two Non-overlapping Sub-arrays Each With Target Sum
+You are given an array of integers arr and an integer target.
+You have to find two non-overlapping sub-arrays of arr each with a sum equal target. There can be multiple answers so you have to find an answer where the sum of the lengths of the two sub-arrays is minimum.
+Return the minimum sum of the lengths of the two required sub-arrays, or return -1 if you cannot find such two sub-arrays.
+
+ 
+
+Example 1:
+Input: arr = [3,2,2,4,3], target = 3
+Output: 2
+Explanation: Only two sub-arrays have sum = 3 ([3] and [3]). The sum of their lengths is 2.
+
+Example 2:
+Input: arr = [7,3,4,7], target = 7
+Output: 2
+Explanation: Although we have three non-overlapping sub-arrays of sum = 7 ([7], [3,4] and [7]), but we will choose the first and third sub-arrays as the sum of their lengths is 2.
+
+Example 3:
+Input: arr = [4,3,2,6,2,3,4], target = 6
+Output: -1
+Explanation: We have only one sub-array of sum = 6.
+ 
+
+Constraints:
+1 <= arr.length <= 105
+1 <= arr[i] <= 1000
+1 <= target <= 108You are given an array of integers arr and an integer target.
+*/
+
+public class Find2NonOverlappingSubarrays {
+    public int minSumOfLengths(int[] arr, int target) {
+        int n = arr.length;
+        Map<Integer, Integer> pos = new HashMap<>();
+        pos.put(0, -1);
+        int s = 0, res = n+1, minL = n;
+
+        for (int i = 0; i < arr.length; i++) {
+            s+=arr[i];
+            if(pos.containsKey(s-target)){
+                int j = pos.get(s-target);
+                int len = i-j;
+                res = Math.min(res, len+(j==-1?n:arr[j]));
+                minL = Math.min(minL, len);
+            }
+            arr[i] = minL;
+            pos.put(s, i);
+        }
+
+        return res == n+1 ? -1: res;
+    }
+}
